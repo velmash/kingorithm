29440a9a54d7ddf8af1fb256426d0478f937c7a5
hyengchan <hyengchan@gmail.com>
2020-12-06 13:56:18 +0900

    3-1) 거스름돈 문제

---
+++ Greedy/거스름돈.py
@@ -0,0 +1,27 @@
+money_type = [500, 100, 50, 10]
+changes = 1260
+count = 0
+
+for coin in money_type:
+    result = changes // coin
+    count += result
+    changes -= coin * result
+
+print(count)
+
+
+
+'''
+# 교재 해설
+n = 1260
+count = 0
+
+# 큰 단위의 화폐부터 차례대로 확인
+coin_type = [500, 100, 50, 10]
+
+for coin in coin_type:
+    count += n // coin # 해당 화폐로 거슬러 줄 수 있는 동전의 개수 세기
+    n %= coin
+
+print(coin)
+'''
