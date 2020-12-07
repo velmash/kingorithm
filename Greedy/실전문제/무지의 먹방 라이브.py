546d991c90a6787fcf4ebee7a81050974830ae26
hyengchan <hyengchan@gmail.com>
2020-12-07 18:03:11 +0900

    4-6) 무지의 먹방 라이브 (정확성 풀이)

--- Greedy/실전문제/무지의 먹방 라이브.py
+++ Greedy/실전문제/무지의 먹방 라이브.py
@@ -1,23 +1,21 @@
-# 나중에 잘하게되면,,
+# 정확성 풀이
+
+from itertools import cycle
+
 
 def solution(food_times, k):
-    answer = 0
-    return answer
+    index_array = cycle([i for i in range(len(food_times))])
+    done = False
+    counter = 0
+    while done is False:
+        index = next(index_array)
+        if sum(food_times) == 0:
+            return -1
+        if food_times[index] > 0:
+            counter += 1
+            food_times[index] -= 1
+        done = True if counter == k + 1 else False
+    return index + 1
 
 
-food_times = [3, 1, 2]
-k = 5
-
-total_time = 0
-pass_time = 0
-
-for i in food_times:
-    total_time += i
-
-for i in range(k):
-    if k > total_time:
-        print(-1)
-    else:
-        if food_times[i % len(food_times)] == 0:
-           print('hi')
-
+print(solution([3, 1, 2], 5))
